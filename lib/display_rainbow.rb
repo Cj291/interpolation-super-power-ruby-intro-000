21a4b93a81d4880d00846cfd7b8fec74b61495f1
Junea Williams <jcw291@gmail.com>
2020-04-07 17:49:40 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,4 +5,3 @@ def display_rainbow(colors)
 #display_rainbow("R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet\n")
 
 end
-display_rainbow(['R: red', 'O: orange', 'Y: yellow', 'G: green', 'B: blue', 'I: indigo', 'V: violet\n'])
